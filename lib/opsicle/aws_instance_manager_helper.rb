5293183af2ad66ce900ee31ed2e2421242424758
Emma Sax <emma.sax4@gmail.com>
2018-02-05 15:27:07 -0600

    Removing duplicated code in all of the commands to manage AWS instances

--- lib/opsicle/aws_instance_manager_helper.rb
+++ lib/opsicle/aws_instance_manager_helper.rb
@@ -1,30 +1,78 @@
-module AwsInstanceManagerHelper
-  def select_layer
-    puts "\nLayers:\n"
-    ops_layers = @opsworks.describe_layers({ :stack_id => @stack.id }).layers
+module Opsicle
+  module AwsInstanceManagerHelper
+    def select_layer
+      puts "\nLayers:\n"
+      ops_layers = @opsworks.describe_layers({ :stack_id => @stack.id }).layers
 
-    layers = []
-    ops_layers.each do |layer|
-      layers << ManageableLayer.new(layer.name, layer.layer_id, @stack, @opsworks, @ec2, @cli)
+      layers = []
+      ops_layers.each do |layer|
+        layers << ManageableLayer.new(layer.name, layer.layer_id, @stack, @opsworks, @ec2, @cli)
+      end
+
+      layers.each_with_index { |layer, index| puts "#{index.to_i + 1}) #{layer.name}" }
+      layer_index = @cli.ask("Layer?\n", Integer) { |q| q.in = 1..layers.length.to_i } - 1
+      layers[layer_index]
     end
 
-    layers.each_with_index { |layer, index| puts "#{index.to_i + 1}) #{layer.name}" }
-    layer_index = @cli.ask("Layer?\n", Integer) { |q| q.in = 1..layers.length.to_i } - 1
-    layers[layer_index]
-  end
+    def select_instances(instances)
+      puts "\nInstances:\n"
+      instances.each_with_index { |instance, index| puts "#{index.to_i + 1}) #{instance.status} - #{instance.hostname}" }
+      instance_indices_string = @cli.ask("Instances? (enter as a comma separated list)\n", String)
+      instance_indices_list = instance_indices_string.split(/,\s*/)
+      instance_indices_list.map! { |instance_index| instance_index.to_i - 1 }
 
-  def select_instances(instances)
-    puts "\nInstances:\n"
-    instances.each_with_index { |instance, index| puts "#{index.to_i + 1}) #{instance.status} - #{instance.hostname}" }
-    instance_indices_string = @cli.ask("Instances? (enter as a comma separated list)\n", String)
-    instance_indices_list = instance_indices_string.split(/,\s*/)
-    instance_indices_list.map! { |instance_index| instance_index.to_i - 1 }
-
-    return_array = []
-    instance_indices_list.each do |index|
-      return_array << instances[index]
+      return_array = []
+      instance_indices_list.each do |index|
+        return_array << instances[index]
+      end
+      return_array
     end
-    return_array
-  end
 
+    def select_deletable_or_stoppable_instances(layer, stop_or_delete)
+      if stop_or_delete == :stop
+        instances = @stack.stoppable_instances(layer)
+        specific_words = { adjective: "stoppable", verb: "stop" }
+      elsif stop_or_delete == :delete
+        instances = @stack.deleteable_instances(layer)
+        specific_words = { adjective: "deletable", verb: "delete" } 
+      end
+      
+      return_array = []
+      if instances.empty?
+        puts "There are no #{specific_words[:adjective]} instances."
+      else
+        puts "\n#{specific_words[:adjective].capitalize} Instances:\n"
+        instances.each_with_index { |instance, index| puts "#{index.to_i + 1}) #{instance.status} - #{instance.hostname}" }
+        instance_indices_string = @cli.ask("Which instances would you like to #{specific_words[:verb]}? (enter as a comma separated list)\n", String)
+        instance_indices_list = instance_indices_string.split(/,\s*/)
+        instance_indices_list.map! { |instance_index| instance_index.to_i - 1 }
+        instance_indices_list.each do |index|
+          return_array << instances[index]
+        end
+      end
+      return_array
+    end
+
+    def stop_or_delete(instances, stop_or_delete)
+      if stop_or_delete == :stop
+        specific_words = { past_tense: "stopped", verb: "stop" }
+      elsif stop_or_delete == :delete
+        specific_words = { past_tense: "deleted", verb: "delete" } 
+      end
+
+      instances.each do |instance|
+        begin
+          if stop_or_delete == :stop
+            @opsworks.stop_instance(instance_id: instance.instance_id)
+          elsif stop_or_delete == :delete
+            @opsworks.delete_instance(instance_id: instance.instance_id)
+          end
+          
+          puts "Successfully #{specific_words[:past_tense]} #{instance.hostname}"
+        rescue
+          puts "Failed to #{specific_words[:verb]} #{instance.hostname}"
+        end
+      end
+    end
+  end
 end
--- lib/opsicle/commands/manage_aws_instances.rb
+++ lib/opsicle/commands/manage_aws_instances.rb
@@ -4,9 +4,11 @@ require "opsicle/manageable_layer"
 require "opsicle/manageable_instance"
 require "opsicle/manageable_stack"
 require "opsicle/creatable_instance"
+require "opsicle/aws_instance_manager_helper"
 
 module Opsicle
-  class CloneInstance
+  class ManageAwsInstances
+    include Opsicle::AwsInstanceManagerHelper
 
     def initialize(environment)
       @client = Client.new(environment)
@@ -15,98 +17,54 @@ module Opsicle
       stack_id = @client.config.opsworks_config[:stack_id]
       @stack = ManageableStack.new(@client.config.opsworks_config[:stack_id], @opsworks)
       @cli = HighLine.new
+
+      puts "Stack ID = #{@stack.id}"
     end
 
-    def execute_clone_instance(options={})
-      puts "Stack ID = #{@stack.id}"
+    def clone_instance(options={})
       layer = select_layer
       all_instances = layer.get_cloneable_instances
       instance_to_clone = select_instances(all_instances)
-      clone_instances(instance_to_clone, options)
-      layer.ami_id = nil
-      layer.agent_version = nil
-    end
 
-    def execute_create_instance(options={})
-      puts "Stack ID = #{@stack.id}"
-      layer = select_layer
-      layer.get_cloneable_instances
-      create_instance(layer, options)
-      layer.ami_id = nil
-      layer.agent_version = nil
-    end
-
-    def execute_stop_instance(options={})
-      puts "Stack ID = #{@stack.id}"
-      layer = select_layer
-      instances_to_stop = select_instances(layer)
-      instances_to_stop.each do |instance|
-        begin
-          @opsworks.stop_instance(instance_id: instance.instance_id)
-          puts "Stopping instance #{instance.hostname}..."
-        rescue
-          puts "Failed to stop #{instance.hostname}"
-        end
-      end
-    end
-
-    def execute_delete_instance(options={})
-      puts "Stack ID = #{@stack.id}"
-      layer = select_layer
-      instances_to_delete = select_instances(layer)
-      instances_to_delete.each do |instance|
-        begin
-          @opsworks.delete_instance(instance_id: instance.instance_id)
-          puts "Successfully deleted #{instance.hostname}"
-        rescue
-          puts "Failed to delete #{instance.hostname}"
-        end
-      end
-    end
-
-    def execute_move_eip(options={})
-      puts "Stack ID = #{@stack.id}"
-      @stack.move_eip
-    end
-
-    def clone_instances(instances, options)
       if options[:'with-defaults']
         instances.each { |instance| instance.clone_with_defaults(options) }
       else
         instances.each { |instance| instance.clone(options) }
       end
+
+      layer.ami_id = nil
+      layer.agent_version = nil
     end
 
-    def create_instance(layer, options)
+    def create_instance(options={})
+      layer = select_layer
+      layer.get_cloneable_instances
       CreatableInstance.new(layer, @stack, @opsworks, @ec2, @cli).create(options)
+      layer.ami_id = nil
+      layer.agent_version = nil
     end
 
-    # def select_layer
-    #   puts "\nLayers:\n"
-    #   ops_layers = @opsworks.describe_layers({ :stack_id => @stack.id }).layers
+    def stop_instance(options={})
+      layer = select_layer
+      instances = select_deletable_or_stoppable_instances(layer, :stop)
+      stop_or_delete(instances)
+    end
 
-    #   layers = []
-    #   ops_layers.each do |layer|
-    #     layers << ManageableLayer.new(layer.name, layer.layer_id, @stack, @opsworks, @ec2, @cli)
-    #   end
+    def delete_instance(options={})
+      layer = select_layer
+      instances = select_deletable_or_stoppable_instances(layer, :delete)
+      stop_or_delete(instances)
+    end
 
-    #   layers.each_with_index { |layer, index| puts "#{index.to_i + 1}) #{layer.name}" }
-    #   layer_index = @cli.ask("Layer?\n", Integer) { |q| q.in = 1..layers.length.to_i } - 1
-    #   layers[layer_index]
-    # end
+    def move_eip(options={})
+      @stack.move_eip
+    end
 
-    # def select_instances(instances)
-    #   puts "\nInstances:\n"
-    #   instances.each_with_index { |instance, index| puts "#{index.to_i + 1}) #{instance.status} - #{instance.hostname}" }
-    #   instance_indices_string = @cli.ask("Instances? (enter as a comma separated list)\n", String)
-    #   instance_indices_list = instance_indices_string.split(/,\s*/)
-    #   instance_indices_list.map! { |instance_index| instance_index.to_i - 1 }
-
-    #   return_array = []
-    #   instance_indices_list.each do |index|
-    #     return_array << instances[index]
-    #   end
-    #   return_array
-    # end
+    def add_tags(options={})
+      layer = select_layer
+      all_instances = layer.get_cloneable_instances
+      instances_to_add_tags = select_instances(all_instances)
+      instances_to_add_tags.each { |instance| instance.add_tags({ add_tags_mode: true }) }
+    end
   end
 end
