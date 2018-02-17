11d05dffb2897a16f0b5ec85f0aa8033e021c569
Vivek Bisen <vivek.bisen@sportngin.com>
2018-02-16 22:12:22 -0600

    beautify tests

--- spec/opsicle/opsworks_adapter_spec.rb
+++ spec/opsicle/opsworks_adapter_spec.rb
@@ -1,33 +1,32 @@
-require "spec_helper"
-require "opsicle"
-require 'gli'
-require "opsicle/user_profile"
+describe Opsicle::OpsworksAdapter do
+  let(:aws_opsworks_client) do
+    double(
+     :aws_opsworks_client,
+     describe_layers: double(:layers, layers: []),
+     start_instance: true
+    )
+  end
+  let(:client) { double(:client, opsworks: aws_opsworks_client) }
 
-module Opsicle
-  describe OpsworksAdapter do
-    before do
-      @layers = double('layers', layers: [])
-      @aws_opsworks_client = double('aws_opsworks_client', describe_layers: @layers, start_instance: true)
-      @client = double('client', opsworks: @aws_opsworks_client)
+  subject { described_class.new(client) }
+
+  describe "#get_layers" do
+    let(:get_layers) { subject.get_layers(:stack_id) }
+
+    it "should gather an array of layers for this opsworks client" do
+      expect(get_layers).to be_empty
     end
 
-    context "#get_layers" do
-      it "should gather an array of layers for this opsworks client" do
-        ops_layers = OpsworksAdapter.new(@client).get_layers(1234)
-        expect(ops_layers).to eq([])
-      end
-
-      it "should call describe_layers on the opsworks client" do
-        expect(@aws_opsworks_client).to receive(:describe_layers).with(stack_id: 1234)
-        ops_layers = OpsworksAdapter.new(@client).get_layers(1234)
-      end
+    it "should call describe_layers on the opsworks client" do
+      expect(aws_opsworks_client).to receive(:describe_layers).with(stack_id: :stack_id)
+      get_layers
     end
+  end
 
-    context "#start_instance" do
-      it "should call start_instance on the opsworks client" do
-        expect(@aws_opsworks_client).to receive(:start_instance).with(instance_id: 1234)
-        ops_layers = OpsworksAdapter.new(@client).start_instance(1234)
-      end
+  describe "#start_instance" do
+    it "should call start_instance on the opsworks client" do
+      expect(aws_opsworks_client).to receive(:start_instance).with(instance_id: :instance_id)
+      subject.start_instance(:instance_id)
     end
   end
 end
