e306c5ad23a2e8a1f4c0f0263dc0d2b29ece723e
Emma Sax <emma.sax4@gmail.com>
2018-05-29 14:13:54 -0500

    Adding additional tests for manageable_stack-spec

--- spec/opsicle/manageable_stack_spec.rb
+++ spec/opsicle/manageable_stack_spec.rb
@@ -1,8 +1,34 @@
 describe Opsicle::ManageableStack do
-  let(:opsworks_adapter) { double('opsworks_adapter', :elastic_ips => true, :transfer_eip => true) }
+  let(:deleteable_instance) do
+    double(:deleteable_instance,
+      auto_scaling_type: nil,
+      status: "stopped",
+      layer_ids: ["456"],
+      elastic_ip: nil
+    )
+  end
+
+  let(:stoppable_instance) do
+    double(:stoppable_instance,
+      elastic_ip: nil,
+      status: "start_failed",
+      layer_ids: ["456"],
+      auto_scaling_type: nil
+    )
+  end
+
+  let(:opsworks_adapter) do
+    double(:opsworks_adapter,
+      elastic_ips: true,
+      instances_by_stack: double(:instances, instances: [deleteable_instance, stoppable_instance]),
+      associate_elastic_ip: true,
+      stack: double(:stack, vpc_id: "123")
+    )
+  end
   let(:stack_id) { '123' }
-  let(:client) { double(:client, stack_id: stack_id, opsworks_adapter: opsworks_adapter) }
-  subject { described_class.new(client) }
+  let(:layer) { double(:layer, layer_id: "456") }
+  
+  subject { described_class.new(stack_id, opsworks_adapter) }
 
   describe "#get_eips" do
     let(:eips) { subject.get_eips }
@@ -13,10 +39,34 @@ describe Opsicle::ManageableStack do
   end
 
   describe "#transfer_eip" do
-    let(:transfer) { subject.transfer_eip }
+    let(:transfer) { subject.transfer_eip({ip_address: true}, "target_instance_id") }
 
     it "should call opsworks adapter to get transfer an EIP" do
       expect(transfer).to eq(true)
+    end
+  end
+
+  describe "#instances" do
+    let(:instances) { subject.instances }
+
+    it "should call opsworks adapter get a list of instances" do
+      expect(instances).to eq([deleteable_instance, stoppable_instance])
+    end
+  end
+
+  describe "#deleteable_instances" do
+    let(:instances) { subject.deleteable_instances(layer) }
+
+    it "should look for any instances that are deleteable" do
+      expect(instances).to eq([deleteable_instance])
+    end
+  end
+
+  describe "#stoppable_instances" do
+    let(:instances) { subject.stoppable_instances(layer) }
+
+    it "should look for any instances that are stoppable" do
+      expect(instances).to eq([stoppable_instance])
     end
   end
 end
